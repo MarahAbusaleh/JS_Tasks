480f2ca86e580b54090d5314d37fb0c226fde569
MarahAbusaleh <marah.abusaleh12@gmail.com>
2023-07-19 14:01:49 +0300

    I solved my today's Task

--- Tasks_18-JULY-2023/Task.js
+++ Tasks_18-JULY-2023/Task.js
@@ -1,14 +1,11 @@
 const tableContainer = document.getElementById('tab');
 
-const startReq = new XMLHttpRequest();
-startReq.open('GET', 'https://fakestoreapi.com/products');
+async function funAsynch() {
 
-startReq.onload = function (){
+    const response = await fetch('https://fakestoreapi.com/products');
+    const Obj = await response.json();
 
-    const Obj = JSON.parse(this.responseText);
-    console.log(Obj);
-    
-    for(let i = 0 ; i < 20 ; i ++){
+    for(let i = 0 ; i < Obj.length ; i ++){
 
         const rowContant = document.createElement('tr');
         tableContainer.appendChild(rowContant);
@@ -22,31 +19,28 @@ startReq.onload = function (){
         rowContant.appendChild(cellContant2);
         cellContant2.textContent = Obj[i].title;
 
-        const cellContant3 = document.createElement('td');
+        const cellContant3 = document.createElement('td');//Description
         rowContant.appendChild(cellContant3);
         cellContant3.textContent = Obj[i].description;
 
-        const cellContant4 = document.createElement('td');
+        const cellContant4 = document.createElement('td');//Category
         rowContant.appendChild(cellContant4);
         cellContant4.textContent = Obj[i].category;
 
-        const cellContant5 = document.createElement('td');
+        const cellContant5 = document.createElement('td');//Rating
         rowContant.appendChild(cellContant5);
         cellContant5.textContent = Obj[i].rating.rate + ' ' + Obj[i].rating.count;
 
-        const cellContant6 = document.createElement('td');
+        const cellContant6 = document.createElement('td');//Price
         rowContant.appendChild(cellContant6);
         cellContant6.textContent = Obj[i].price;
 
-        const cellContant7 = document.createElement('td');
+        const cellContant7 = document.createElement('td');//Image
         rowContant.appendChild(cellContant7);
         const cellContant7Image = document.createElement('img');
         cellContant7.appendChild(cellContant7Image);
         cellContant7Image.src = Obj[i].image;
-        cellContant7Image.style.width = '100px';
-
+        cellContant7Image.style.width = '100px';7
     }
-
 }
-
-startReq.send();
+funAsynch();
